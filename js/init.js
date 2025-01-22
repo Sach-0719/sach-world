7453d30ebd60436cce1245d7286b15850c326fed
Sach-0719 <kenyui1357@gmail.com>
2025-01-22 16:13:16 +0900

    Update init.js

--- js/init.js
+++ js/init.js
@@ -6,3 +6,7 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.parallax');
+  var instances = M.Parallax.init(elems);
+});
