e9169d73fb318318e3d907c937ac1e87392f197e
Sach-0719 <kenyui1357@gmail.com>
2025-01-23 17:50:01 +0900

    ok!
    oohaba

--- js/init.js
+++ js/init.js
@@ -6,7 +6,3 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.parallax');
-  var instances = M.Parallax.init(elems);
-});
